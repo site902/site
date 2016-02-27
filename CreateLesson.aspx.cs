b86efad6ad5764022166d92bf0b718ea955fcb3e
ororor2000 <ororor2000@gmail.com>
2016-02-27 20:40:30 +0200

    ddddddd

--- CreateLesson.aspx.cs
+++ CreateLesson.aspx.cs
@@ -7,16 +7,34 @@ using System.Web.UI.WebControls;
 
 public partial class CreateLesson : System.Web.UI.Page
 {
-
+    string fileName = "Database1.mdb";
+    string table = "Lessons";
+  
     public string user = "";
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        
         if (Session["username"] == null)
         {
             Response.Redirect("Default.aspx");
         }
-
+        
         user += Session["fname"].ToString() + " " + Session["lname"].ToString();
+
+        string name = user;
+        string date = Request.Form["date"];
+
+        string hrsS = Request.Form["hrs"];
+        int hrs = int.Parse(hrsS);
+
+        string minS = Request.Form["min"];
+        int min = int.Parse(minS);
+
+        string sql = "INSERT INTO " + table + "(studentName,LessonDate,hour,minute) VALUES ('" + name + "','" + date + "','" + hrs + "','" + min + "')";
+
+        MyAdoHelper.DoQuery(fileName, sql);
+
+        Response.Redirect("Default.aspx");
     }
 }
