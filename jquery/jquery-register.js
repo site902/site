324b958d892fca1eaa407800f80facad198c980e
Dan Peled <Dan Peled>
2016-02-27 20:14:00 +0200

    dd

--- jquery/jquery-register.js
+++ jquery/jquery-register.js
@@ -1,37 +1,5 @@
 ﻿/// <reference path="jquery.min.js" />
 
-//var main = function () {
-//    var username = $('#username');
-//    var fname = $('#fname');
-//    var lname = $('#lname');
-//    var phone = $('#phone');
-//    var ephone = $('#e-phone');
-//    var password = $('#password');
-
-//    username.keypress(function () {
-//        //לעשות שימוש באג'קס
-//    });
-//    phone.keydown(function () {
-//        if (!isNaN(phone.text)) {
-//            if (phone.text.length < 9) {
-//                ephone.text = "המספר קצר מדי";
-//            }
-//            else {
-//                if (phone.text.length > 9) {
-//                    ephone.text = "המספר ארוך מדי";
-//                }
-//                else {
-//                    if (phone.text.length === 9) {
-//                        ephone.text = "";
-//                    }
-//                }
-//            }
-//        }
-//        else {
-//            ephone.text = "הוזנו תווים לא חוקיים";
-//        }
-//    });
-//}
 
 $(document).ready(function () {
     var username = $('#username');
