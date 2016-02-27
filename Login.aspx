0fa66c5de03b2c41f0665c86a120f4144e932061
ororor2000 <ororor2000@gmail.com>
2016-02-27 18:39:01 +0200

    ddd

---
+++ Login.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <form name="login" method="post" action="">
+
+        <h3>התחברות:</h3>
+
+        <p><%=incor %></p>
+
+        <table>
+            <tr>
+                <td><label>שם משתמש: </label></td>
+                <td><input type="text" name="username" /></td>
+            </tr>
+
+            <tr>
+                <td><label>סיסמא: </label></td>
+                <td><input type="password" name="password" /></td><
+            </tr>
+
+            <tr>
+                <td><input type="submit" name="submit" /></td>
+                <td><input type="reset" name="reset" /></td>
+            </tr>
+        </table>
+
+    </form>
+</asp:Content>
+
---
+++ Login.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class Login : System.Web.UI.Page
+{
+    public string incor = "";
+    string fileName = "DB.mdb";
+    string tableName = "Table";
+
+    public bool IsUserAndPaswordExist(string username, string password)
+    {
+        string fileName = "Database1.mdb";
+        string tableName = "Userdata";
+
+        string isExUsername = "SELECT * FROM " + tableName + " WHERE username = '" + username + "' AND password_f = '" + password + "'";
+
+        return MyAdoHelper.IsExist(fileName, isExUsername);
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Form["submit"] != null)
+        {
+            string user = Request.Form["username"];
+            string password = Request.Form["password"];
+
+            string isExUsername = "SELECT * FROM " + tableName + " WHERE username = '" + user + "' AND password_f = '" + password + "'";
+
+             if (IsUserAndPaswordExist(user, password))
+             {
+                 DataTable dt = MyAdoHelper.ExecuteDataTable(fileName, isExUsername);
+
+                 if (((bool)dt.Rows[0]["isAdmin"]))
+                {
+                    Session["admin"] = "True";
+                }
+                else
+                {
+                    Session["admin"] = "False";
+                }
+
+                Session["username"] = user;
+
+             }
+             else
+             {
+                 incor = "user or pasword are incorrect";
+             }
+        }        
+    }
+}
