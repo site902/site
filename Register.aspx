b6520c5e363a1ec830e6bf812b5c5b9c4e6cd4c1
Dan Peled <Dan Peled>
2016-02-27 20:11:21 +0200

    dd

--- Register.aspx
+++ Register.aspx
@@ -1,6 +1,7 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Register.aspx.cs" Inherits="Register" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <script src="jquery/jquery-register.js"></script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
     <form action="" method="post" runat="server">
@@ -8,24 +9,29 @@
         <tr>
             <th>שם משתמש</th>
             <td><input type="text" name="username" id="username" /></td>
+            <td><b class="regError" id="e-username"></b></td>
         </tr>
         <tr>
             <th>שם פרטי</th>
             <td><input type="text" name="fname" id="fname" /></td>
+            <td><b class="regError" id="e-fname"></b></td>
         </tr>
 
         <tr>
-            <th>שם משפחה:  </th>
+            <th>שם משפחה</th>
             <td><input type="text" name="lname" id="lname" /></td>
+            <td><b class="regError" id="e-lname"></b></td>
         </tr>
 
         <tr>
             <th>טלפון נייד</th>
-            <td><input type="text" name="phone" id="phone" /></td>
+            <td dir="ltr">05 <input type="text" name="phone" id="phone" /></td>
+            <td><b class="regError" id="e-phone"></b></td>
         </tr>
         <tr>
             <th>סיסמא</th>
             <td><input type="password" name="password" id="password" /></td>
+            <td><b class="regError" id="e-pass"></b></td>
         </tr>
         <tr>
             <th>מין</th>
---
+++ jquery/jquery-register.js
@@ -0,0 +1,75 @@
+﻿/// <reference path="jquery.min.js" />
+
+//var main = function () {
+//    var username = $('#username');
+//    var fname = $('#fname');
+//    var lname = $('#lname');
+//    var phone = $('#phone');
+//    var ephone = $('#e-phone');
+//    var password = $('#password');
+
+//    username.keypress(function () {
+//        //לעשות שימוש באג'קס
+//    });
+//    phone.keydown(function () {
+//        if (!isNaN(phone.text)) {
+//            if (phone.text.length < 9) {
+//                ephone.text = "המספר קצר מדי";
+//            }
+//            else {
+//                if (phone.text.length > 9) {
+//                    ephone.text = "המספר ארוך מדי";
+//                }
+//                else {
+//                    if (phone.text.length === 9) {
+//                        ephone.text = "";
+//                    }
+//                }
+//            }
+//        }
+//        else {
+//            ephone.text = "הוזנו תווים לא חוקיים";
+//        }
+//    });
+//}
+
+$(document).ready(function () {
+    var username = $('#username');
+    var fname = $('#fname');
+    var lname = $('#lname');
+    var phone = $('#phone');
+    var ephone = $('#e-phone');
+    var password = $('#password');
+
+    username.keypress(function () {
+        //לעשות שימוש באג'קס
+    });
+
+    phone.keyup(function () {
+        var phoneValue = phone.val();
+        if (!isNaN(phoneValue)) {
+            if (phoneValue.length < 8 && phoneValue.length != 0) {
+                ephone.text("המספר קצר מדי");
+            }
+            else {
+                if (phoneValue.length > 8) {
+                    ephone.text("המספר ארוך מדי");
+                }
+                else {
+                    if (phoneValue.length === 8) {
+                        ephone.text("");
+                    }
+                    else {
+                        if (phoneValue.length === 0) {
+                            ephone.text("יש להזין מספר")
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            ephone.text("הוזנו תווים לא חוקיים");
+        }
+    });
+});
+
