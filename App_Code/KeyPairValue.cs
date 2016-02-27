615d8354744ddeaf17425e50b962fd27d2d377c0
ororor2000 <ororor2000@gmail.com>
2016-02-27 18:10:57 +0200

    dd

---
+++ App_Code/KeyPairValue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coffe
+{
+    class KeyValuePair<TKey, TValue>
+    {
+        private TKey key;
+        private TValue value;
+
+        public KeyValuePair(TKey key, TValue val)
+        {
+            this.key = key;
+            this.value = val;
+        }
+
+        public TKey Key
+        {
+            get { return this.key; }
+            set { this.key = value; }
+        }
+
+        public TValue Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Key: " + this.key);
+            Console.WriteLine("Value: " + this.value);
+        }
+        
+        public void AddToDictionary(Dictionary<TKey, TValue> a)
+        {
+            a.Add(this.key, this.value);
+        }
+    }
+}
