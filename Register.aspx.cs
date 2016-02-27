c0aaa8d31fe7f01be4e1dfeab69786e7d86c86de
ororor2000 <ororor2000@gmail.com>
2016-02-27 18:18:00 +0200

    dd

---
+++ Register.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        List<KeyValuePair<string, string>> fields = new List<KeyValuePair<string, string>>(6);
+
+        string[] keys = 
+        {
+            "username",
+            "fname",
+            "lname",
+            "phone",
+            "password",
+            "gender"
+        };
+
+        for (int i = 0; i < fields.Count; i++)
+        {
+            string key = keys[i];
+            string value = Request.Form[key];
+
+            
+        }
+    }
+}
